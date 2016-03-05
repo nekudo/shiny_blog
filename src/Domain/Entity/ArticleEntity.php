f7e37929c073a7cb60c9e8f9ee57fb83c66d9f30
Simon Samtleben <simon@nekudo.com>
2016-03-05 16:19:16 +0100

    Optimize meta-data handling

--- src/Domain/Entity/ArticleEntity.php
+++ src/Domain/Entity/ArticleEntity.php
@@ -37,4 +37,32 @@ class ArticleEntity extends BaseEntity
         }
         return $excerpt;
     }
+
+    /**
+     * Returns article title formatted as defined in config.
+     *
+     * @return string
+     */
+    public function getTitle() : string
+    {
+        $seoConfig = $this->config['seo']['article'];
+        if (empty($seoConfig['title'])) {
+            return $this->title;
+        }
+        return sprintf($seoConfig['title'], $this->title);
+    }
+
+    /**
+     * Returns article description formatted as defined in config.
+     *
+     * @return string
+     */
+    public function getDescription() : string
+    {
+        $seoConfig = $this->config['seo']['page'];
+        if (empty($seoConfig['description'])) {
+            return $this->description;
+        }
+        return sprintf($seoConfig['description'], $this->description);
+    }
 }
