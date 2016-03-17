8c2258b4a8089bd3a635862f837596649b1c4de5
Simon Samtleben <simon@nekudo.com>
2016-03-17 21:35:35 +0100

    Add output of artice meta and categories

--- themes/nekudo/article.php
+++ themes/nekudo/article.php
@@ -1,10 +1,21 @@
-<div class="article">
-    <?php echo $article->getContent(); ?>
-</div>
-<?php if ($article->hasCategories()): ?>
-    <div class="categories">
-    <?php foreach ($article->getCategories() as $category): ?>
-        <a href="<?php echo $category['link']; ?>"><?php echo $category['name']; ?></a>
-    <?php endforeach; ?>
+<article>
+    <header>
+        <h2><?php echo $article->getTitle(); ?></h2>
+        <div class="entry-meta">
+            <?php echo $article->getDate(); ?> / by
+            <?php echo $article->getAuthor(); ?>
+        </div>  
+    </header>
+    <div class="entry-content">
+        <?php echo $article->getContent(); ?>
     </div>
-<?php endif; ?>
+    <footer>
+        <?php if ($article->hasCategories()): ?>
+            <div class="categories">
+            <?php foreach ($article->getCategories() as $category): ?>
+                <a href="<?php echo $category['link']; ?>"><?php echo $category['name']; ?></a>
+            <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+    </footer>
+</article>
