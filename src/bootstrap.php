dfdef3a68a26b9c5ec886a2c7e528c18554ecf6d
Simon Samtleben <simon@nekudo.com>
2016-03-23 21:15:29 +0100

    Drop composer and include vendor libs in project

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,7 +2,20 @@
 declare(strict_types=1);
 namespace Nekudo\ShinyBlog;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+use Nekudo\ShinyBlog\Core\Psr4Autoloader;
+
+$appRoot = __DIR__ . '/../src/';
+$vendorRoot = __DIR__ . '/../vendor/';
+
+require_once $appRoot . 'Core/Autoloader.php';
+require_once $vendorRoot . 'nikic/fast-route/src/functions.php';
+require_once $vendorRoot . 'erusev/parsedown/Parsedown.php';
+require_once $vendorRoot . 'erusev/parsedown-extra/ParsedownExtra.php';
+
+$loader = new Psr4Autoloader;
+$loader->register();
+$loader->addNamespace('FastRoute\\', __DIR__ . '/../vendor/nikic/fast-route/src/');
+$loader->addNamespace('Nekudo\\ShinyBlog\\', __DIR__ . '/../src/');
 
 $config = require 'config.php';
 $blog = new ShinyBlog($config);
