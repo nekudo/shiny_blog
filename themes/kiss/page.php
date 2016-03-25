ccb32f168e34636ed96e16b4470361094eca7508
Simon Samtleben <simon@samtleben.me>
2016-03-25 19:33:24 +0100

    Minor improvements

--- themes/kiss/page.php
+++ themes/kiss/page.php
@@ -1,5 +1,7 @@
 <article class="page">
-    <h1><?php echo $page->getTitle(); ?></h1>
+    <?php if(!empty($page->getTitle())): ?>
+        <h1><?php echo $page->getTitle(); ?></h1>
+    <?php endif; ?>
     <div class="page-content">
         <?php echo $page->getContent(); ?>
     </div>
